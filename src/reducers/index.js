52a067e275d2db62246c953f6b37f6387acc8aba
Peng Ye <yep0608@hotmail.com>
2019-08-25 20:22:15 +1000

    redux added

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+export default function counterReducer(state={count:0}, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return Object.assign({}, state, {
+        count: state.count + 1,
+      });
+    case 'DECREMENT':
+      return {...state, count:state.count -1};
+    default:
+      return state;
+  }
+}
